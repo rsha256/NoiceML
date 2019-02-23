aa1d9353c1ba5e07530870944bd4dc7a2efd87ed
Samvit123 <samvitagarwal03@gmail.com>
2019-02-23 11:47:47 -0500

    enter

---
+++ BackEnd/speech.py
@@ -0,0 +1,31 @@
+import io
+import os
+
+# Imports the Google Cloud client library
+from google.cloud import speech
+from google.cloud.speech import enums
+from google.cloud.speech import types
+
+
+# Instantiates a client
+client = speech.SpeechClient()
+
+# The name of the audio file to transcribe
+file_name = "Google.flac"
+
+# Loads the audio into memory
+with io.open(file_name, 'rb') as audio_file:
+    content = audio_file.read()
+    audio = speech.types.RecognitionAudio(content=content)
+
+config = speech.types.RecognitionConfig(
+    encoding=enums.RecognitionConfig.AudioEncoding.FLAC,
+    sample_rate_hertz=44100,
+    language_code='en-US'
+)
+
+# Detects speech in the audio file
+response = client.recognize(config, audio)
+
+for result in response.results:
+    print('Transcript: {}'.format(result.alternatives[0].transcript))
