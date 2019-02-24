bc35f0bfb749e2872c3cb81f4f2404bed8f6f1a6
Samvit123 <samvitagarwal03@gmail.com>
2019-02-24 07:52:12 -0500

    enter

--- app/livespeech2.py
+++ app/livespeech2.py
@@ -1,8 +1,12 @@
-from watson_developer_cloud import SpeechToTextV1
+import speech_recognition as sr  
 
-speech_to_text = SpeechToTextV1(
-    iam_apikey='EovOzTbczUH5DsI7ZzI83KGr6mIODNA8KY3lGSkbJ32S',
-    url='https://stream.watsonplatform.net/speech-to-text/api'
-)
-
-recognize(self, audio, content_type=None, model=None, language_customization_id=None, acoustic_customization_id=None, base_model_version=None, customization_weight=None, inactivity_timeout=None, keywords=None, keywords_threshold=None, max_alternatives=None, word_alternatives_threshold=None, word_confidence=None, timestamps=None, profanity_filter=None, smart_formatting=None, speaker_labels=None, customization_id=None, grammar_name=None, redaction=None, **kwargs)
+# obtain audio from the microphone  
+r = sr.Recognizer()  
+r.energy_threshold = 10000
+with sr.Microphone() as source:  
+    while True:
+        audio = r.listen(source)  
+        try:  
+            print(r.recognize_google(audio))
+        except:
+            pass
